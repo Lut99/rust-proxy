97e3394621e1b9c5bb4bc027d1c84efa848d3ee2
Lut99 <mullertim99@gmail.com>
2022-10-17 19:30:10 +0200

    Added file + neat description for the SourceText and SourceRef structs.

---
+++ proxy-lang/src/source.rs
@@ -0,0 +1,21 @@
+//  SOURCE.rs
+//    by Lut99
+// 
+//  Created:
+//    17 Oct 2022, 19:29:02
+//  Last edited:
+//    17 Oct 2022, 19:29:43
+//  Auto updated?
+//    Yes
+// 
+//  Description:
+//!   Defines the `SourceText` and `SourceRef` structs, which can both be
+//!   used to link AST nodes back to their parsed location in the source
+//!   text. Furthermore, they are both [nom](https://github.com/Geal/nom)
+//!   compatible and can thus be used as easy input wrappers (much like
+//!   `LocatedSpan` in [nom_locate](https://github.com/Geal/nom).
+// 
+
+
+/***** LIBRARY *****/
+
